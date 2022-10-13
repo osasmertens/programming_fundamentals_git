59f89d896569366ca4d4c54761411df8860510e6
osasmertens <r0605937@student.thomasmore.be>
2022-10-13 19:50:42 +0200

    Added a variable and a getter method.

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,13 @@
 public class TestClass {
-    
+
+    private String name;
+
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+    }
+
+    public String getName(){
+        return this.name;
     }
 }
