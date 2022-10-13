27c4cf6e6f6fcc5e4caa7c2677e6e030b8dff762
osasmertens <r0605937@student.thomasmore.be>
2022-10-13 19:52:41 +0200

    Added a constructor.

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,6 +2,10 @@ public class TestClass {
 
     private String name;
 
+    public TestClass(){
+        this.name="";
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
